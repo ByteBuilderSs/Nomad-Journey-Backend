f590df7471f8fa08687eb9fae076e7e3dba7972b
sinsinnati <sinazamani9364@gmail.com>
2023-03-17 17:01:40 +0330

    announcement_app

---
+++ announcement/views.py
@@ -0,0 +1,40 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Announcement
+from .serializers import AnnouncementSerializer
+
+
+@api_view(['GET'])
+def GetAnnouncementList(request):
+    announcements = Announcement.objects.all()
+    serializer = AnnouncementSerializer(announcements, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def GetSingleAnnouncement(request, pk):
+    announcement = Announcement.objects.get(id=pk)
+    serializer = AnnouncementSerializer(announcement, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def CreateAnnouncement(request):
+    serializer = AnnouncementSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def EditAnnouncement(request, pk):
+    announcement = Announcement.objects.get(id=pk)
+    serializer = AnnouncementSerializer(instance=announcement, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def DeleteAnnouncement(request, pk):
+    announcement = Announcement.objects.get(id=pk)
+    announcement.delete()
+    return Response('Item successfully deleted!')
+
