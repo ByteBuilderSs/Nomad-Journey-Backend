ebaa6aefaf248e3b632d8647de87cd00682a4d17
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-03-20 11:14:43 +0330

    [fix] : fix CORS

--- NormandJourney/settings.py
+++ NormandJourney/settings.py
@@ -151,4 +151,4 @@ CROS_ALLOW_CREDENTIALS = False
 #     "http://localhost:8080",
 #     "http://127.0.0.1:8080"
 # ]
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000" , "http://127.0.0.1:8000"]
