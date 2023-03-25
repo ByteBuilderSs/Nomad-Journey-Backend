de4a183de6a3047ae7ed4c3ec01104bf576781d2
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-03-25 13:29:21 +0430

    change database

--- NormandJourney/settings.py
+++ NormandJourney/settings.py
@@ -96,9 +96,12 @@ WSGI_APPLICATION = 'NormandJourney.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'nomadjourney',        
+        'USER': 'postgres',        
+        'PASSWORD': 'aysa1380',        
+        'HOST': 'localhost',        
+        'PORT': '5432',}
 }
 
 
