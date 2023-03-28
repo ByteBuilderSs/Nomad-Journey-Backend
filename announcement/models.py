6cc29fab9f878b68fabff6f3e4ee5372964d6eb6
sinsinnati <sinazamani9364@gmail.com>
2023-03-28 15:57:40 +0330

    improving_anc

--- announcement/models.py
+++ announcement/models.py
@@ -20,41 +20,57 @@ class Announcement(models.Model):
     THIRTEEN_TR = 13
     FOURTEEN_TR = 14
     FIFTEEN_TR = 15
-    TRAVELERS_COUNT_CHOICES = [
-        (ONE_TR, _("1")),
-        (TWO_TR, _("2")),
-        (THREE_TR, _("3")),
-        (FOUR_TR, _("4")),
-        (FIVE_TR, _("5")),
-        (SIX_TR, _("6")),
-        (SEVENT_TR, _("7")),
-        (EIGHT_TR, _("8")),
-        (NINE_TR, _("9")),
-        (TEN_TR, _("10")),
-        (ELEVEN_TR, _("11")),
-        (TWELVE_TR, _("12")),
-        (THIRTEEN_TR, _("13")),
-        (FOURTEEN_TR, _("14")),
-        (FIFTEEN_TR, _("15"))
-    ]
+    TRAVELERS_COUNT_CHOICES = (
+        (ONE_TR, _('1')),
+        (TWO_TR, _('2')),
+        (THREE_TR, _('3')),
+        (FOUR_TR, _('4')),
+        (FIVE_TR, _('5')),
+        (SIX_TR, _('6')),
+        (SEVENT_TR, _('7')),
+        (EIGHT_TR, _('8')),
+        (NINE_TR, _('9')),
+        (TEN_TR, _('10')),
+        (ELEVEN_TR, _('11')),
+        (TWELVE_TR, _('12')),
+        (THIRTEEN_TR, _('13')),
+        (FOURTEEN_TR, _('14')),
+        (FIFTEEN_TR, _('15'))
+    )
+
     announcer = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        default=None
+        default=None,
+        related_name='announcer_anc'
     )
 
-    country = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    STATUS_CHOICES = (
+        ('P', _('Pending')),
+        ('A', _('Accepted')),
+        ('D', _('Done')),
+        ('E', _('Expired'))
+    )
+
+    anc_city = models.CharField(max_length=100)
+    anc_country = models.CharField(max_length=100)
     arrival_date = models.DateField()
     departure_date = models.DateField()
-    arrival_date_is_flexible = models.BooleanField(null=True)
-    departure_date_is_flexible = models.BooleanField(null=True)
-    message = models.TextField(max_length=500, null=True)
-    timestamp_created = models.DateTimeField(auto_now_add=True)
+    anc_status = models.CharField(choices=STATUS_CHOICES, default='P', max_length=1)
+    arrival_date_is_flexible = models.BooleanField(null=True, blank=True)
+    departure_date_is_flexible = models.BooleanField(null=True, blank=True)
+    anc_description = models.TextField(max_length=500, null=True, blank=True)
+    anc_timestamp_created = models.DateTimeField(auto_now_add=True)
     travelers_count = models.IntegerField(choices=TRAVELERS_COUNT_CHOICES, null=True, blank=True)
 
+    volunteer_hosts = models.ManyToManyField(User, related_name='hosts_anc')
+
+    main_host = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        default=None,
+        related_name='main_host_anc'
+    )
+
     def __str__(self):
-        return 'This is an announcement with ID ' + str(self.id) + '.'
-
-
-# test
+        return 'This is an announcement with ID ' + str(self.id) + ' from user with ID ' + str(self.announcer.id) + '.'
--- announcement/views.py
+++ announcement/views.py
@@ -1,11 +1,10 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .models import Announcement
 from .serializers import AnnouncementSerializer
-import json
-from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
