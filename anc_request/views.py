78cb63ea33c58436c4a9c8e47408cc953842703e
sinsinnati <sinazamani9364@gmail.com>
2023-03-31 12:03:55 +0330

    startapp request

---
+++ anc_request/views.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import AncRequest
+from .serializers import AncRequestSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetRequestsOnAnnouncement(request, anc_id):
+    anc_requests = AncRequest.objects.filter(req_anc=anc_id)
+    serializer = AncRequestSerializer(anc_requests, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetRequestsOfHost(request, host_id):
+    anc_requests = AncRequest.objects.filter(host=host_id)
+    serializer = AncRequestSerializer(anc_requests, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def GetRequestsOfHost(request):
+    pass
