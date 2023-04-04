816e6a3187a9ef14b969f15465b6548d4151f66b
sinsinnati <sinazamani9364@gmail.com>
2023-04-04 23:39:06 +0330

    [add] accept and reject request

--- anc_request/urls.py
+++ anc_request/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('get-requests-on-announcement/<str:anc_id>', GetRequestsOnAnnouncement),
     path('get-requests-of-host/<str:host_id>', GetRequestsOfHost),
 
-    
+    path('create-request/<str:anc_id>', CreateRequest),
+
+    path('accept-request/<str:req_id>', AcceptRequest),
+    path('reject-request/<str:req_id>', RejectRequest)
 ]
--- anc_request/views.py
+++ anc_request/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import AncRequest
 from .serializers import AncRequestSerializer
+from announcement.models import Announcement
 
 
 @api_view(['GET'])
@@ -28,4 +29,22 @@ def CreateRequest(request, anc_id):
         serializer.save()
     return Response(serializer.data)
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def AcceptRequest(request, req_id):
+    req = AncRequest.objects.get(id=req_id)
+    base_announcement = Announcement.objects.get(id=req.req_anc)
+    dest_announcement = base_announcement
+    dest_announcement.anc_status = 'A'
+    dest_announcement.main_host = req.host
+    serializer = AncRequestSerializer(instance=base_announcement, data=dest_announcement)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def RejectRequest(request, req_id):
+    req = AncRequest.objects.get(id=req_id)
+    req.delete()
+    return Response('Request deleted successfully!')
