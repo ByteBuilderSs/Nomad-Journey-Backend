4ca18a2b7827f01d45ad967e723a4a6aa48e9507
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-04-09 21:02:21 +0430

    [fix] : fix blog serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,9 +2,17 @@ from rest_framework import serializers
 from .models import Blog , Tag
 
 class BlogSerializer(serializers.ModelSerializer):
+    tags_name = serializers.SerializerMethodField('get_tag_name') 
     class Meta:
         model = Blog
-        fields = ("__all__")
+        fields = ['uid','created_at','updated_at','author','blog_title','blog_text','json_data','main_image_64','slug','tags_name']
+    
+    def get_tag_name(self,obj):
+        tags_name_list = []
+        for t in obj.tags.all():
+            tags_name_list.append(t.tag_name)
+        return tags_name_list
+
 
 
 class TagSerializer(serializers.ModelSerializer):
