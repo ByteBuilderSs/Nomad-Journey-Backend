be828572e401a02d245af3043de6b542029b6979
sinsinnati <sinazamani9364@gmail.com>
2023-04-17 15:10:58 +0330

    [add] apis for city & country

---
+++ utils/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('get-countries/', GetCountries),
+    path('get-cities-of-country/<str:rec_id>', GetCitiesOfCountry)
+]
--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import City
+from .serializers import CountrySerializer, CitySerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetCountries(request):
+    countries = City.objects.all().distinct('country')
+    serializer = CountrySerializer(countries, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetCitiesOfCountry(request, rec_id):
+    single_city = City.objects.get(id=rec_id)
+    cities = City.objects.filter(country=single_city.country)
+    serializer = CitySerializer(cities, many=True)
+    return Response(serializer.data)
