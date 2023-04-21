77d68b21eff1d5c6a8f72e67438a0c4c3e2c4e15
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-04-21 20:49:07 +0430

    [fix] : fix serializers related to city-country

--- accounts/views.py
+++ accounts/views.py
@@ -173,52 +173,52 @@ class UserProfileEdit1(APIView):
 
 class UserProfileEdit2(APIView):
     def patch(self , request , username):
-        try:
-            body = json.loads(request.body.decode('utf-8'))
-            user = User.objects.filter(username = username)
-            if len(user) == 0:
-                return Response({
-                    'data': {},
-                    'message':'invalid username'
-                }, status = status.HTTP_400_BAD_REQUEST )
-            if request.user.id != user[0].id:
-                return Response({
-                    'data': {},
-                    'message':'you are not authorized to do this'
-                }, status = status.HTTP_400_BAD_REQUEST )
-
-            # if body.get('User_city') is None:
-            #     return Response({
-            #         'data': {},
-            #         'message':'city field should not be null'
-            #     }, status = status.HTTP_400_BAD_REQUEST )
-            if body.get('User_country')  is None:
-                return Response({
-                    'data': {},
-                    'message':'country field should not be null'
-                }, status = status.HTTP_400_BAD_REQUEST )
-            if body.get('User_postal_code')  is None:
-                return Response({
-                    'data': {},
-                    'message':'postal code field should not be null'
-                }, status = status.HTTP_400_BAD_REQUEST )
-            serializer = UserProfileEdit2Serializer(user[0] , data = body , partial = True)
-            if not serializer.is_valid():
-                return Response({
-                    'data': serializer.errors,
-                    'message':'something went wrong'
-                } , status = status.HTTP_400_BAD_REQUEST)
-            serializer.save()
-            return Response({
-                'data': serializer.data,
-                'message' : 'user updated successfully'
-            } , status = status.HTTP_201_CREATED)
-        except Exception as e:
-            print(e) 
+        # try:
+        body = json.loads(request.body.decode('utf-8'))
+        user = User.objects.filter(username = username)
+        if len(user) == 0:
             return Response({
                 'data': {},
-                'message':'something went wrong'
+                'message':'invalid username'
             }, status = status.HTTP_400_BAD_REQUEST )
+        if request.user.id != user[0].id:
+            return Response({
+                'data': {},
+                'message':'you are not authorized to do this'
+            }, status = status.HTTP_400_BAD_REQUEST )
+
+        # if body.get('User_city') is None:
+        #     return Response({
+        #         'data': {},
+        #         'message':'city field should not be null'
+        #     }, status = status.HTTP_400_BAD_REQUEST )
+        # if body.get('User_country')  is None:
+        #     return Response({
+        #         'data': {},
+        #         'message':'country field should not be null'
+        #     }, status = status.HTTP_400_BAD_REQUEST )
+        if body.get('User_postal_code')  is None:
+            return Response({
+                'data': {},
+                'message':'postal code field should not be null'
+            }, status = status.HTTP_400_BAD_REQUEST )
+        serializer = UserProfileEdit2Serializer(user[0] , data = body , partial = True)
+        if not serializer.is_valid():
+            return Response({
+                'data': serializer.errors,
+                'message':'something went wrong'
+            } , status = status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response({
+            'data': serializer.data,
+            'message' : 'user updated successfully'
+        } , status = status.HTTP_201_CREATED)
+        # except Exception as e:
+        #     print(e) 
+        #     return Response({
+        #         'data': {},
+        #         'message':'something went wrong'
+        #     }, status = status.HTTP_400_BAD_REQUEST )
 
 class UserProfileEdit3(APIView):
     def patch(self , request , username):
