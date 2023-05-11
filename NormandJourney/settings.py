d1d6f284eef9b350516d767f78cf6101791e1173
sinsinnati <sinazamani9364@gmail.com>
2023-05-11 22:16:38 +0330

    [fix] change db user pass

--- NormandJourney/settings.py
+++ NormandJourney/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
         'NAME': 'nomadjourney',        
-        'USER': 'postgres',        
-        'PASSWORD': 'aysa1380',        
+        'USER': 'sina',        
+        'PASSWORD': 'sin2023',        
         'HOST': 'localhost',        
         'PORT': '5432',}
 }
