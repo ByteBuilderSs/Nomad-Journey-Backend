619d26e43cac18f3d3d71485f5b59608151840b5
sinsinnati <sinazamani9364@gmail.com>
2023-05-11 13:13:05 +0330

    [add] getpostswithlike api

--- like_post/views.py
+++ like_post/views.py
@@ -2,7 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from blog.models import Blog
-from .serializers import LikePostSerializer
+from .serializers import *
+from accounts.models import User
 
 
 @api_view(['POST'])
@@ -13,4 +14,19 @@ def CreateLike(request, post_id):
 
     if serializer.is_valid():
         serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetPostsWithLike(request, post_id):
+    post = Blog.objects.get(id=post_id)
+    likers_ids = Like.objects.filter(post=post_id).values('liker')
+    likers_objs = User.objects.filter(id__in=likers_ids)
+
+    ref = Like.objects.filter(post=post_id).first()
+
+    setattr(ref, 'post', post)
+    setattr(ref, 'likers', likers_objs)
+
+    serializer = PostWithLikesSerializer(ref)
     return Response(serializer.data)
