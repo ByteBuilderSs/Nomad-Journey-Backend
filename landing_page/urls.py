aae40a5f9ea172592a61001fb71b36c333cb629c
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-05-18 18:14:03 +0330

    [add] : add random shit api

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,8 +3,10 @@ from .views import *
 
 
 urlpatterns = [ path('most-rated-hosts' , MostRatedHost.as_view()),
-                path('most-visited-cities' , MostVisitedCities.as_view())
+                path('most-visited-cities' , MostVisitedCities.as_view()),
+                path('random-shit' , RandomShit.as_view())
 ]
 
 #http://127.0.0.1:8000/api/v1/landing-page/most-rated-hosts
 #http://127.0.0.1:8000/api/v1/landing-page/most-visited-cities
+#http://127.0.0.1:8000/api/v1/landing-page/random-shit
