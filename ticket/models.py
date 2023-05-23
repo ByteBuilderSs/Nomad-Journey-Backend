704ced0d8c4ef5b00c986551265b39870c99a4e3
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-05-23 11:39:21 +0330

    [fix] : add more fields random shit

--- ticket/models.py
+++ ticket/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from announcement.models import Announcement
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
@@ -7,6 +8,7 @@ class Message(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    ans_id = models.ForeignKey(Announcement , on_delete=models.CASCADE , related_name='ans')
     def __str__(self):
         return self.message
 
