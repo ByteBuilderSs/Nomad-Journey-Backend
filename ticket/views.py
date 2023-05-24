268d91bc3ac7b62b54c76a67c0220cb18f416d95
aysa_nia <aysa_mayahinia@comp.iust.ac.ir>
2023-05-24 18:31:12 +0330

    [fix] : fix and add new view for seen and unssen messages

--- ticket/views.py
+++ ticket/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.shortcuts import get_object_or_404
 from .serializers import MessageSerializer
-from .models import Message
+from .models import Message 
 from accounts.models import User
 
 
@@ -14,7 +14,7 @@ def get_id(username):
     user = User.objects.get(username = username)
     return user.id
 
-class MessageListView(APIView):
+class MessageUnseenListView(APIView):
     def get(self , request,sender_username , receiver_username):
         try:
             if request.user.username!= sender_username and request.user.username != receiver_username:
@@ -38,10 +38,32 @@ class MessageListView(APIView):
                 'message':'something went wrong'
             }, status = status.HTTP_400_BAD_REQUEST )
 
+class MessageGeneralListView(APIView):
+    def get(self , request,sender_username , receiver_username):
+        try:
+            if request.user.username!= sender_username and request.user.username != receiver_username:
+                return Response({
+                    'data': {},
+                    'message':'you are not authorized to do this'
+                }, status = status.HTTP_400_BAD_REQUEST )
+            messages = Message.objects.filter(sender = get_id(sender_username), receiver = get_id(receiver_username))
+            serializer = MessageSerializer(messages, many=True, context={'request': request})
+            for message in messages:
+                message.is_read = True
+                message.save()
+            return Response({
+                'data':serializer.data,
+                'message' : 'messages fetched successfully'
+            } , status = status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e) 
+            return Response({
+                'data': {},
+                'message':'something went wrong'
+            }, status = status.HTTP_400_BAD_REQUEST )
+
     def post(self , request , sender_username):
         try:
-            print("request.user.username: " , request.user.username)
-            print("sender_username: " , sender_username)
             if request.user.username!= sender_username:
                 return Response({
                     'data': {},
