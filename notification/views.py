30f35c733ec45f0966b428d7f07c53418a2bf183
sinsinnati <sinazamani9364@gmail.com>
2023-07-08 12:41:17 +0330

    [fix] notif created_at time zone warning

--- notification/views.py
+++ notification/views.py
@@ -3,11 +3,13 @@ from rest_framework.decorators import api_view
 from datetime import datetime, timedelta
 from .serializers import NotificationSerializer
 from .models import Notification
+from django.utils import timezone
 
 
 @api_view(['GET'])
 def UserNotifications(request, user_id):
     one_day_ago = datetime.now() - timedelta(days=1)
+    one_day_ago = timezone.make_aware(one_day_ago, timezone.utc)
     notifs = Notification.objects.filter(user_receiver=user_id).filter(created_at__gte=one_day_ago)
     serializer = NotificationSerializer(notifs, many=True)
     return Response(serializer.data)
